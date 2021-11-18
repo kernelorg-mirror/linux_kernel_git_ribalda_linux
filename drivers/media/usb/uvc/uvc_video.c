65e2b5f3b367fc411f00b615dfc92b3b6eb8a684
Ricardo Ribalda <ribalda@chromium.org>
2021-11-18 22:09:06 +0000

    media: uvc: Extend documentation of uvc_video_clock_decode()
    Make a explicit reference to uvc 1.5, explaining how the algorithm
    supports the different behaviour of uvc 1.1 and 1.5.
    
    Tested-by: hn.chen <hn.chen@sunplusit.com>
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/usb/uvc/uvc_video.c
+++ drivers/media/usb/uvc/uvc_video.c
@@ -517,6 +517,9 @@ uvc_video_clock_decode(struct uvc_streaming *stream, struct uvc_buffer *buf,
 	/*
 	 * To limit the amount of data, drop SCRs with an SOF identical to the
 	 * previous one.
+	 * This filtering is also needed for supporting UVC 1.5. Where all the
+	 * data packages of the same frame contains the same sof. In that case
+	 * only the first one will match the host_sof.
 	 */
 	dev_sof = get_unaligned_le16(&data[header_size - 2]);
 	if (dev_sof == stream->clock.last_sof)
