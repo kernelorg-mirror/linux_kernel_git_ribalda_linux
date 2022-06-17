40723afa206635e2148d05b550d88010ecca5bd8
Ricardo Ribalda <ribalda@chromium.org>
2022-06-18 01:56:04 +0200

    media: uvcvideo: Implement mask for V4L2_CTRL_TYPE_MENU
    Replace the count with a mask field that lets us choose not only the max
    value, but also the minimum value and what values are valid in between.
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    Suggested-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -2675,7 +2675,7 @@ static const struct uvc_control_mapping uvc_ctrl_power_line_mapping_limited = {
 	.v4l2_type	= V4L2_CTRL_TYPE_MENU,
 	.data_type	= UVC_CTRL_DATA_TYPE_ENUM,
 	.menu_info	= power_line_frequency_controls_limited,
-	.menu_count	= ARRAY_SIZE(power_line_frequency_controls_limited),
+	.menu_mask	= BIT_MASK(ARRAY_SIZE(power_line_frequency_controls_limited)),
 };
 
 static const struct uvc_device_info uvc_ctrl_power_line_limited = {
--- drivers/media/usb/uvc/uvc_v4l2.c
+++ drivers/media/usb/uvc/uvc_v4l2.c
@@ -80,7 +80,7 @@ static int uvc_ioctl_ctrl_map(struct uvc_video_chain *chain,
 			goto free_map;
 		}
 
-		map->menu_count = xmap->menu_count;
+		map->menu_mask = BIT_MASK(xmap->menu_count);
 		break;
 
 	default:
