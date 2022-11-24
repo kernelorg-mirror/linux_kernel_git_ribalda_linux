c1e5e28b38b16b6f34d51fe76771c01f45aa4b19
Ricardo Ribalda <ribalda@chromium.org>
2022-11-24 10:28:05 +0100

    earlycon: Let users set the clock frequency
    Some platforms, namely AMD Picasso, use non standard uart clocks (48M),
    witch makes it impossible to use with earlycon.
    
    Let the user select its own frequency.
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/tty/serial/earlycon.c
+++ drivers/tty/serial/earlycon.c
@@ -120,8 +120,14 @@ static int __init parse_options(struct earlycon_device *device, char *options)
 	}
 
 	if (options) {
+		char *uartclk;
+
 		if (kstrtouint(options, 0, &device->baud) < 0)
 			pr_warn("[%s] unsupported earlycon baud rate option\n",
+				options);
+		uartclk = strchr(options, ',');
+		if (uartclk && kstrtouint(uartclk, 0, &port->uartclk) < 0)
+			pr_warn("[%s] unsupported earlycon uart clkrate option\n",
 				options);
 		length = min(strcspn(options, " ") + 1,
 			     (size_t)(sizeof(device->options)));
@@ -141,7 +147,8 @@ static int __init register_earlycon(char *buf, const struct earlycon_id *match)
 		buf = NULL;
 
 	spin_lock_init(&port->lock);
-	port->uartclk = BASE_BAUD * 16;
+	if (!port->uartclk)
+		port->uartclk = BASE_BAUD * 16;
 	if (port->mapbase)
 		port->membase = earlycon_map(port->mapbase, 64);
 
