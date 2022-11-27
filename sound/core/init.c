172dd468e86beed35b12304a00467e7a4dcc067c
Ricardo Ribalda <ribalda@chromium.org>
2022-11-27 23:24:00 +0100

    ALSA: core: Fix deadlock when shutdown a frozen userspace
    If the user space is frozen, we cannot wait for it to complete.
    
    This fixes:
    
    [   84.943749] Freezing user space processes ... (elapsed 0.111 seconds) done.
    [  246.784446] INFO: task kexec-lite:5123 blocked for more than 122 seconds.
    [  246.819035] Call Trace:
    [  246.821782]  <TASK>
    [  246.824186]  __schedule+0x5f9/0x1263
    [  246.828231]  schedule+0x87/0xc5
    [  246.831779]  snd_card_disconnect_sync+0xb5/0x127
    ...
    [  246.889249]  snd_sof_device_shutdown+0xb4/0x150
    [  246.899317]  pci_device_shutdown+0x37/0x61
    [  246.903990]  device_shutdown+0x14c/0x1d6
    [  246.908391]  kernel_kexec+0x45/0xb9
    
    And
    
    [  246.893222] INFO: task kexec-lite:4891 blocked for more than 122 seconds.
    [  246.927709] Call Trace:
    [  246.930461]  <TASK>
    [  246.932819]  __schedule+0x5f9/0x1263
    [  246.936855]  ? fsnotify_grab_connector+0x5c/0x70
    [  246.942045]  schedule+0x87/0xc5
    [  246.945567]  schedule_timeout+0x49/0xf3
    [  246.949877]  wait_for_completion+0x86/0xe8
    [  246.954463]  snd_card_free+0x68/0x89
    ...
    [  247.001080]  platform_device_unregister+0x12/0x35
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>

--- sound/core/init.c
+++ sound/core/init.c
@@ -9,6 +9,7 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/file.h>
+#include <linux/freezer.h>
 #include <linux/slab.h>
 #include <linux/time.h>
 #include <linux/ctype.h>
@@ -573,6 +574,13 @@ void snd_card_disconnect_sync(struct snd_card *card)
 		return;
 	}
 
+#ifdef CONFIG_FREEZER
+	if (pm_freezing) {
+		dev_err(card->dev, "Userspace is frozen, skipping sync\n");
+		return;
+	}
+#endif
+
 	spin_lock_irq(&card->files_lock);
 	wait_event_lock_irq(card->remove_sleep,
 			    list_empty(&card->files_list),
@@ -658,6 +666,15 @@ int snd_card_free(struct snd_card *card)
 	ret = snd_card_free_when_closed(card);
 	if (ret)
 		return ret;
+
+#ifdef CONFIG_FREEZER
+	/*
+	 * If userspace is frozen the wait from completion will never end.
+	 */
+	if (pm_freezing)
+		return 0;
+#endif
+
 	/* wait, until all devices are ready for the free operation */
 	wait_for_completion(&released);
 
